3489871d78c85aa3d750eb20dd1b6e69290a91ef
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-26 09:50:07 +0100

    created main body components

---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Section1 from './Body/Section1';
+
+export default class Body extends React.Component {
+  render() {
+    return (
+      <div>
+        <Section1 />
+      </div>
+    );
+  }
+}
---
+++ src/components/Body/Section1.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class Section1 extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 text-center">
+           <p>ello</p>
+          </div>
+          <div className="col-md-4 text-center">
+            <p>ello</p>
+          </div>
+          <div className="col-md-4 text-center">
+            <p>ello</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
