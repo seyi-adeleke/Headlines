269d8a69845ebb098f7dd1573e0e3675f67a90cd
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-27 11:09:00 +0100

    imported body component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Footer from './Footer';
 import Header from './Header';
 import Body from './Body';
 
 export default class Layout extends React.Component {
-
   render() {
     return (
       <div>
