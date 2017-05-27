59f6d6ef29a90fba57c9ae04953cb63a1658bf41
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-27 17:27:04 +0100

    refactored code

--- src/components/Body/Section1.jsx
+++ src/components/Body/Section1.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import axios from 'axios';
 import SelectNewsSource from './SelectNewsSource.jsx';
 import SelectSortOrder from './SelectSortOrder.jsx';
+import Section2 from './Section2.jsx';
 
 
 export default class Section1 extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { source: 'TechCrunch', sort: 'top', info: '' };
+    this.state = { source: 'TechCrunch', sort: 'top', info: '', showResults: false };
     this.newSource = this.newSource.bind(this);
     this.newSort = this.newSort.bind(this);
     this.getLatestNews = this.getLatestNews.bind(this);
@@ -15,9 +16,8 @@ export default class Section1 extends React.Component {
   getLatestNews() {
     axios.get(`https://newsapi.org/v1/articles?source=${this.state.source}&sortBy=${this.state.sort}&apiKey=213327409d384371851777e7c7f78dfe`)
       .then((response) => {
-        this.setState({ info: response.data });
-        console.log(response.data);
-
+        this.setState({ info: response.data,
+          showResults: true });
       })
       .catch((error) => {
         console.log(error);
@@ -31,18 +31,26 @@ export default class Section1 extends React.Component {
   }
   render() {
     return (
-      <div className="container">
-        <div className="row text-center">
-          <div className="col-md-4">
-            <SelectNewsSource getSource={ newState => this.newSource(newState)} />
-          </div>
-          <div className="col-md-4">
-            <SelectSortOrder getSort={ newState => this.newSort(newState)} />
-          </div>
-          <div className="col-md-4" style={{ marginTop: 20 }}>
-            <button onClick={this.getLatestNews} className="btn search-btn"><b>Get News</b></button>
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <SelectNewsSource getSource={newState => this.newSource(newState)} />
+            </div>
+            <div className="col-md-4">
+              <SelectSortOrder getSort={newState => this.newSort(newState)} />
+            </div>
+            <div className="col-md-4" style={{ marginTop: 20 }}>
+              <button
+                onClick={this.getLatestNews}
+                className="btn search-btn"
+              >
+                <b>Get News</b>
+              </button>
+            </div>
           </div>
         </div>
+        {this.state.showResults ? <Section2 data={this.state.info} /> : null}
       </div>
     );
   }
