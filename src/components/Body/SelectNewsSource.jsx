5e64d7b8957dc23ceeccba60a8e00c915cf2d991
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-29 15:50:07 +0100

    added additional sources

--- src/components/Body/SelectNewsSource.jsx
+++ src/components/Body/SelectNewsSource.jsx
@@ -22,10 +22,27 @@ export default class SelectNewsSource extends React.Component {
             onChange={this.handleChange}
             className="form-control"
           >
+            <option>Associated-Press</option>
+            <option>Bbc-News</option>
+            <option>Bloomberg</option>
+            <option>Business-Insider</option>
+            <option>CNN</option>
+            <option>Engadget</option>
+            <option>Espn</option>
+            <option>Fortune</option>
+            <option>Independent</option>
+            <option>Ign</option>
+            <option>Mashable</option>
+            <option>Metro</option>
+            <option>Mirror</option>
+            <option>Newsweek</option>
+            <option>Polygon</option>
+            <option>Recode</option>
+            <option>Reuters</option>
+            <option>TalkSport</option>
             <option>TechCrunch</option>
-            <option>Top</option>
-            <option>Latest</option>
-            <option>Popular</option>
+            <option>TechRadar</option>
+            <option>Time</option>
           </select>
         </div>
       </div>
