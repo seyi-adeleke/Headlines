ec5bb1978a347f39dd9ed9bbc94d5ec5f1f8a05f
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-30 12:57:14 +0100

    refactored code

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Header from './Header.jsx';
 import Body from './Body.jsx';
 
-export default class Layout extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <br />
-        <br />
-        <br />
-        <br />
-        <Body />
-      </div>
-    );
-  }
-}
+
+const Layout = props => (
+  <div>
+    <Header />
+    <br />
+    <br />
+    <br />
+    {props.children}
+  </div>
+);
+
+Layout.propTypes = {
+  children: PropTypes.oneOfType([
+    React.PropTypes.arrayOf(React.PropTypes.node),
+    React.PropTypes.node
+  ]).isRequired
+};
+export default Layout;
