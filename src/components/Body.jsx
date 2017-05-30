57ca982d13cd5b00320414ff1948eaeb8dbecbe4
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-30 12:54:42 +0100

    added line breaks

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Section1 from './Body/Section1.jsx';
 
+
 export default class Body extends React.Component {
   render() {
     return (
       <div>
         <Section1 />
+        <br />
+        <br />
       </div>
     );
   }
