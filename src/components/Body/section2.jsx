e81207faab44eff7d66914aa588d3521ce12b999
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-30 12:58:55 +0100

    code cleanup

--- src/components/Body/section2.jsx
+++ src/components/Body/section2.jsx
@@ -12,14 +12,43 @@ export default class Section2 extends React.Component {
       <div className="container">
         <hr />
         <div className="row">
-          <div className="col-md-6 col-md-offset-3">
-            <ul>
-              {
-                Object.keys(obj).map((key)=>{
-                  return <li>{(obj[key].description)}</li>;
-                  })
-                }
-            </ul>
+          <div className="col-md-12">
+            <div className="panel panel-primary panel-table">
+              <div className="panel-heading">
+                <h3 className="panel-title text-center">
+                  The Latest Headlines from {this.props.data.source}
+                </h3>
+              </div>
+              <div className="panel-body">
+                <table className="table table-striped table-bordered table-list">
+                  <thead>
+                    <tr>
+                      <th><i className="fa-4x fa fa-photo" /></th>
+                      <th>Author</th>
+                      <th>Title</th>
+                      <th>Description</th>
+                      <th>Go to Article</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                    Object.keys(obj).map(key =>
+                    <tr>
+                      <td><img alt="news" style={{ width: 105 }} src={(obj[key].urlToImage)} /></td>
+                      <td>{(obj[key].author)}</td>
+                      <td>{(obj[key].title)}</td>
+                      <td>{(obj[key].description)}</td>
+                      <td><a rel="noopener noreferrer" target="_blank" href={(obj[key].url)}>
+                        Go To Article
+                      </a>
+                      </td>
+                    </tr>
+                    )
+                    }
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </div>
