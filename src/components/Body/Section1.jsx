e236e21a468cb29ed6811d5474fb84b62565d571
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-06-01 16:24:27 +0100

    refactor to flux

--- src/components/Body/Section1.jsx
+++ src/components/Body/Section1.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import axios from 'axios';
 import SelectNewsSource from './SelectNewsSource.jsx';
 import SelectSortOrder from './SelectSortOrder.jsx';
 import Section2 from './Section2.jsx';
+import store from '../../store/articlesStore';
+import action from '../../Actions/actions';
 
-
+/*
 export default class Section1 extends React.Component {
   constructor(props) {
     super(props);
@@ -56,3 +57,55 @@ export default class Section1 extends React.Component {
     );
   }
 }
+*/
+export default class Section1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { source: 'TechCrunch', sort: 'top', info: '', showResults: false };
+    this.newSource = this.newSource.bind(this);
+    this.newSort = this.newSort.bind(this);
+    this.getNews = this.getNews.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+  componentDidMount() {
+    store.addChangeListener(this.onChange);
+  }
+  onChange() {
+    this.setState({ info: store.getList(), showResults: true });
+  }
+  getNews() {
+    action.receiveArticle(this.state.source, this.state.sort);
+  }
+  newSource(newState) {
+    this.setState({ source: newState });
+  }
+  newSort(newState) {
+    this.setState({ sort: newState });
+  }
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <SelectNewsSource getSource={newState => this.newSource(newState)} />
+            </div>
+            <div className="col-md-4">
+              <SelectSortOrder getSort={newState => this.newSort(newState)} />
+            </div>
+            <div className="col-md-4" style={{ marginTop: 20 }}>
+              <button
+                onClick={this.getNews}
+                className="btn search-btn"
+              >
+                <b>Get News</b>
+              </button>
+            </div>
+          </div>
+        </div>
+        {this.state.showResults ? <Section2 data={this.state.info} /> : null}
+      </div>
+    );
+  }
+}
+
--- src/components/Body/section2.jsx
+++ src/components/Body/section2.jsx
@@ -7,7 +7,9 @@ export default class Section2 extends React.Component {
     this.state = { data: this.props.data };
   }
   render() {
-    const obj = this.props.data.articles;
+    const obj = this.props.data.list;
+    const sourceName = obj.source;
+    const sourceData = obj.articles;
     return (
       <div className="container">
         <hr />
@@ -16,14 +18,14 @@ export default class Section2 extends React.Component {
             <div className="panel panel-primary panel-table">
               <div className="panel-heading">
                 <h3 className="panel-title text-center">
-                  The Latest Headlines from {this.props.data.source}
+                  The Latest Headlines from {sourceName}
                 </h3>
               </div>
               <div className="panel-body">
                 <table className="table table-striped table-bordered table-list">
                   <thead>
                     <tr>
-                      <th><i className="fa-4x fa fa-photo" /></th>
+                      <th><i className="fa-3x fa fa-photo" /></th>
                       <th>Author</th>
                       <th>Title</th>
                       <th>Description</th>
@@ -32,13 +34,19 @@ export default class Section2 extends React.Component {
                   </thead>
                   <tbody>
                     {
-                    Object.keys(obj).map(key =>
+                    Object.keys(sourceData).map(key =>
                     <tr>
-                      <td><img alt="news" style={{ width: 105 }} src={(obj[key].urlToImage)} /></td>
-                      <td>{(obj[key].author)}</td>
-                      <td>{(obj[key].title)}</td>
-                      <td>{(obj[key].description)}</td>
-                      <td><a rel="noopener noreferrer" target="_blank" href={(obj[key].url)}>
+                      <td>
+                        <img
+                          alt="Not Available"
+                          style={{ width: 105 }}
+                          src={(sourceData[key].urlToImage)}
+                        />
+                      </td>
+                      <td>{(sourceData[key].author)}</td>
+                      <td>{(sourceData[key].title)}</td>
+                      <td>{(sourceData[key].description)}</td>
+                      <td><a rel="noopener noreferrer" target="_blank" href={(sourceData[key].url)}>
                         Go To Article
                       </a>
                       </td>
