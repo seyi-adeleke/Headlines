c810f1af535872f8795caa0db5b9289c943606a0
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-06-01 16:21:45 +0100

    created handleview and handleserver functions

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -1,3 +1,19 @@
-import Dispatcher from 'flux';
+const Dispatcher = require('flux').Dispatcher;
 
-export default new Dispatcher();
+const AppDispatcher = new Dispatcher();
+
+AppDispatcher.handleViewAction = function (action) {
+  this.dispatch({
+    source: 'VIEW_ACTION',
+    action
+  });
+};
+
+AppDispatcher.handleServerAction = function (action) {
+  this.dispatch({
+    source: 'SERVER_ACTION',
+    action
+  });
+};
+
+module.exports = AppDispatcher;
